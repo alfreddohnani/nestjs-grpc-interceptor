e1437117b98b30bc81618aa2a11966f999367156
Alfred Doh-Nani <alfreddohnani@hotmail.com>
2024-03-23 08:12:26 +0000

    chore: show how to use getMap

--- src/product/product.interceptor.ts
+++ src/product/product.interceptor.ts
@@ -13,6 +13,9 @@ export class ProductInterceptor implements NestInterceptor {
     console.log('GETS CALLED');
     const meta = context.switchToRpc().getContext<Metadata>();
 
+    // access the metadata as an object by calling getMap
+    console.log(meta.getMap());
+
     // You can modify, add and remove metadata here in the interceptor which
     // The metadata will be passed to the next handler(ProductController)
     meta.add('Set-Cookie', 'yummy_cookie=choco');
